4c280ec3f9148c50d7766a672648ca044efba628
Luis Miguel <luismikg@gmail.com>
2025-10-01 00:24:03 -0600

    :tada: first commit

---
+++ app/movies/[id].tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+
+const MovieDetails = () => {
+    return (
+        <View>
+            <Text>Welcome!</Text>
+        </View>
+    )
+}
+
+export default MovieDetails
+const styles = StyleSheet.create({})
